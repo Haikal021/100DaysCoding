7035051e53cfabba27ce6611bbea4c1d27a33c48
Haikal021 <100192113+Haikal021@users.noreply.github.com>
2023-10-10 02:42:09 -1200

    Add files via upload

---
+++ dayy2.java
@@ -0,0 +1,10 @@
+public class dayy2 {
+    public static void main(String[] args) {
+        String nama = "Haikal";
+        String alamat = "Tubo Sendana";
+
+        System.out.println("Halo, perkenalkan diri saya:");
+        System.out.println("Nama: " + nama);
+        System.out.println("Alamat: " + alamat);
+    }
+}
