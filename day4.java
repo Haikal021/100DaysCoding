ae2cdd3287269cf08e19f2a2ad19b0e9519bc374
Haikal021 <100192113+Haikal021@users.noreply.github.com>
2023-10-13 01:11:56 -1200

    Add files via upload

---
+++ day4.java
@@ -0,0 +1,14 @@
+public class day4 {
+          public static void main(String[] args) {
+                    String nama = "Haikal";
+                    String alamat = "Tubo Sendana";
+                    int umur = 20;
+                    double ipk = 3.79;
+
+                    System.out.println("Halo, perkenalkan diri saya:");
+                    System.out.println("Nama: " + nama);
+                    System.out.println("Alamat: " + alamat);
+                    System.out.println("umur saya " + umur + " tahun");
+                    System.out.println("ipk saya " + ipk);
+          }
+}
