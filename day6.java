51dc2124bba53bf1781a10647373127b69e4b619
Haikal021 <100192113+Haikal021@users.noreply.github.com>
2023-10-14 02:18:44 -1200

    Add files via upload

---
+++ day6.java
@@ -0,0 +1,19 @@
+public class day6 {
+          public static void main(String[] args) {
+                    String nama = "Haikal";
+                    String alamat = "Tubo Sendana";
+                    int umur = 20;
+                    double ipk = 3.79;
+                    char kelas = 'H';
+                    long KodePos = 91455;
+
+                    System.out.println("Halo, perkenalkan diri saya:");
+                    System.out.println("Nama: " + nama);
+                    System.out.println("Alamat: " + alamat);
+                    System.out.println("umur saya " + umur + " tahun");
+                    System.out.println("ipk saya " + ipk);
+                    System.out.println("Kelas " + kelas + " Informatika");
+                    System.out.println("Kode Pos :  " + KodePos);
+          }
+
+}
