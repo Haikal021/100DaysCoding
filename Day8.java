58aba634b4e75a52f3d8ae1d2b680c24328ead54
Haikal021 <100192113+Haikal021@users.noreply.github.com>
2023-10-16 06:17:42 -1200

    Add files via upload

---
+++ Day8.java
@@ -0,0 +1,25 @@
+public class Day8 {
+          public static void main(String[] args) {
+                    String nama = "Haikal";
+                    String alamat = "Tubo Sendana";
+                    int umur = 20;
+                    double ipk = 3.79;
+                    char kelas = 'H';
+                    long KodePos = 91455;
+                    boolean mahasiswa = true;
+                    float TB = 164.0f;
+                    byte tgl_lahir = 25;
+
+                    System.out.println("Halo, perkenalkan diri saya:");
+                    System.out.println("Nama: " + nama);
+                    System.out.println("Alamat: " + alamat);
+                    System.out.println("umur saya " + umur + " tahun");
+                    System.out.println("ipk saya " + ipk);
+                    System.out.println("Kelas Informatika " + kelas);
+                    System.out.println("Kode Pos :  " + KodePos);
+                    System.out.println("status mahasiswa " + mahasiswa);
+                    System.out.println("tinggi Badan saya " + TB);
+                    System.out.println("tanggal lahir saya " + tgl_lahir);
+          }
+
+}
