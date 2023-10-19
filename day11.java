ad1067544669abd35125dc5fffd9e351c74bd6a1
Haikal021 <100192113+Haikal021@users.noreply.github.com>
2023-10-19 03:37:04 -1200

    Add files via upload

---
+++ day11.java
@@ -0,0 +1,7 @@
+public class day11 {
+          public static void main(String[] args) {
+                    String numberStr = "123";
+                    int number = Integer.parseInt(numberStr);
+                    System.out.println("Hasil konversi: " + number);
+          }
+}
