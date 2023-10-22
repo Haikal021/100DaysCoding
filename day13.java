ef7b52daa2ed32d8c428b1bdf9336ef4ed0a15d1
Haikal021 <100192113+Haikal021@users.noreply.github.com>
2023-10-22 05:54:16 -1200

    non primitif

---
+++ day13.java
@@ -0,0 +1,14 @@
+public class day13 {
+          public static void main(String[] args) {
+
+                    String[][] data = {
+                                        { "Haikal", "20" },
+                                        { "Ikal", "15" }
+                    };
+
+                    for (int r = 0; r < data.length; r++) {
+                              System.out.println("Nama: " + data[r][0]);
+                              System.out.println("Umur: " + data[r][1]);
+                    }
+          }
+}
