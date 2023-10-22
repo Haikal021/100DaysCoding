51b26cc54daf2493a5725b026b3cde289193576a
Haikal021 <100192113+Haikal021@users.noreply.github.com>
2023-10-22 06:08:35 -1200

    non primitif

---
+++ day14.java
@@ -0,0 +1,14 @@
+public class day14 {
+          public static void main(String[] args) {
+
+                    String[] nama = { "Haikal", "Ikal", };
+                    char[] jenisKelamin = { 'L', 'L', };
+
+                    for (int i = 0; i < nama.length; i++) {
+                              System.out.println("Mahasiswa " + (i + 1));
+                              System.out.println("Nama: " + nama[i]);
+                              System.out.println("Jenis Kelamin: " + jenisKelamin[i]);
+                              System.out.println();
+                    }
+          }
+}
