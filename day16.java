2dfdc651b30d0de42bbb517d775740cc273b844b
Haikal021 <100192113+Haikal021@users.noreply.github.com>
2023-10-29 22:49:12 +0700

    Add files via upload

---
+++ day16.java
@@ -0,0 +1,19 @@
+public class day16 {
+    public static void main(String[] args) {
+        int angka = 123;
+        String angkaStr = String.valueOf(angka);
+        
+
+        double angkaDesimal = 3.14159;
+        String angkaDesimalStr = String.valueOf(angkaDesimal);
+   
+
+        boolean benar_Salah = true;
+        String benar_SalahStr = String.valueOf(benar_Salah);
+        
+
+        System.out.println(" tipe data String dalam bentuk String : " + angkaStr);
+        System.out.println("Tipe data double dalam bentuk String : " + angkaDesimalStr);
+        System.out.println("Tipe data boolean dalam bentuk String : " + benar_SalahStr);
+    }
+}
