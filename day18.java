02831eb2ddf44cb45881247b0ef2d470a6444591
Haikal021 <100192113+Haikal021@users.noreply.github.com>
2023-10-31 22:11:08 +0700

    Add files via upload

---
+++ day18.java
@@ -0,0 +1,16 @@
+public class day18 {
+    public static void main(String[] args) {
+        int[] angka = {1, 2, 3, 4, 5};
+        String[] nama = {"Haikal", "Ikal"};
+        
+        for (int angkaInt : angka) {
+            System.out.print(angkaInt + " ");
+        }
+        
+        System.out.println();
+        
+        for (String namaString : nama) {
+            System.out.print(namaString + " ");
+        }
+    }
+}
