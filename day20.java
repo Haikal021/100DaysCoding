1c3796fb58dbd7a76e88ee95f1345f358c0c3b98
Haikal021 <100192113+Haikal021@users.noreply.github.com>
2023-10-31 23:00:23 +0700

    Add files via upload

---
+++ day20.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class day20 {
+    public static void main(String[] args) {
+        ArrayList<String> nama = new ArrayList<String>();
+        nama.add("Haikal");
+        nama.add("Ikal");
+        nama.add("Haikal Ikal");
+
+        for (String namaString : nama) {
+            System.out.println(namaString);
+        }
+
+    }
+}
