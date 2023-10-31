1395bf9990cabc52328c31d5c4c6d5b4fe00f629
Haikal021 <100192113+Haikal021@users.noreply.github.com>
2023-10-31 22:33:11 +0700

    Add files via upload

---
+++ day19.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class day19 {
+    public static void main(String[] args) {
+        ArrayList<Integer> angka = new ArrayList<Integer>();
+        angka.add(1);
+        angka.add(2);
+        angka.add(3);
+        angka.add(4);
+        angka.add(5);
+
+        for (int angkaInt : angka) {
+            System.out.print(angkaInt + "\n"); 
+        }
+    }
+}
