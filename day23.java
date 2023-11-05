fb54444ca9576ab5fe3965c0e8402766996456ed
Haikal021 <100192113+Haikal021@users.noreply.github.com>
2023-11-05 16:24:28 +0700

    Add files via upload

---
+++ day23.java
@@ -0,0 +1,9 @@
+public class day23 {
+    public static void main(String[] args) {
+        String angkaStr = "12345";
+        try {
+            int angka = Integer.parseInt(angkaStr);
+            System.out.println("Angka: " + angka);
+        } 
+    }
+}
