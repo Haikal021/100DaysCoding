0f640219f1a308845bdd12444fc0e7227c86b9a6
Haikal021 <100192113+Haikal021@users.noreply.github.com>
2023-11-15 10:29:54 +0700

    Add files via upload
    UK

---
+++ segitiga.py
@@ -0,0 +1,12 @@
+a = 5
+b = 12
+c = 7 
+d = 9
+t = 10
+keliling = a + b + c + d
+luas = 0.5 * (a+b) * t
+tinggi = 2 * luas / (a+b)
+
+print ("keliling : ", keliling)
+print ("Luas : ", luas)
+print ("tinggi : ", tinggi)
